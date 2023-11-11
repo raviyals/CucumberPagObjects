307ca66fa04c2f5ad36bc7047d4fc11cfdeaf0f0
raviyals <ravi.yalavarthi@gmail.com>
2023-11-11 15:11:29 -0500

    Added test results to github actions - fixed

--- src/test/resources/test_results_script.sh
+++ src/test/resources/test_results_script.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 
-TEST_RESULTS_LOCATION="${1:~/home/runner/work/CucumberPagObjects/CucumberPagObjects/target/surefire-reports}"
+TEST_RESULTS_LOCATION="${1:-/home/runner/work/CucumberPagObjects/CucumberPagObjects/target/surefire-reports}"
 TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "<testng-results")
 echo "SKIPPED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $2}')"
 echo "FAILED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $4}')"
 echo "TOTAL_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $6}')"
 echo "PASSED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $8}')"
-echo "TEST_DURATION=$(echo ${TEST_TIME_IN_SECONDS})"
