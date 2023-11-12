65bd2de7919689542f4187db22d044c3427d8740
raviyals <ravi.yalavarthi@gmail.com>
2023-11-11 20:44:45 -0500

    Publishing test run data to prometheus running in local host

--- src/test/resources/test_results_script.sh
+++ src/test/resources/test_results_script.sh
@@ -3,7 +3,13 @@
 TEST_RESULTS_LOCATION="${1:-/home/runner/work/CucumberPagObjects/CucumberPagObjects/target/surefire-reports}"
 TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "<testng-results")
 echo "SKIPPED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $2}')"
-echo "github_actions_skipped_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $2}')" | curl --data-binary @- ${{secrets.PUSHGATEWAY_URL}}/metrics/job/github_actions
 echo "FAILED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $4}')"
 echo "TOTAL_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $6}')"
 echo "PASSED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $8}')"
+
+cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
+github_actions_skipped_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $2}')
+github_actions_failed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $4}')
+github_actions_total_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $6}')
+github_actions_passed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $8}')
+EOF
