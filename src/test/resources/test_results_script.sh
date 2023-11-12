ed20d2647a4b12f3047bffd093f82be8f667eee3
raviyals <ravi.yalavarthi@gmail.com>
2023-11-11 21:13:44 -0500

    Added tags to action metrics

--- src/test/resources/test_results_script.sh
+++ src/test/resources/test_results_script.sh
@@ -8,8 +8,8 @@ echo "TOTAL_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $6}')"
 echo "PASSED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $8}')"
 
 cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-github_actions_skipped_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $2}')
-github_actions_failed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $4}')
-github_actions_total_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $6}')
-github_actions_passed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $8}')
+github_actions_skipped_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $2}')
+github_actions_failed_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $4}')
+github_actions_total_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $6}')
+github_actions_passed_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{print $8}')
 EOF
